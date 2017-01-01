019b2d22266e94513a09b39b614e5ea3a4f1a51e
codybraun <codybraun@gmail.com>
2016-12-31 22:39:16 -0700

    mostly working

--- rec_app/views.py
+++ rec_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.template import loader
 import requests 
+import json
 import random
 from rec_app.models import *
 from django.contrib.auth import authenticate, login, logout
@@ -18,34 +19,32 @@ def user(request):
   if request.method=="POST":
     user = User.objects.create()
     user.save()
-    return HttpResponse(str(model_to_dict(user)))
+    return JsonResponse(model_to_dict(user))
   else:
     return HttpResponse("Invalid")
 
+@csrf_exempt
 def product(request):
   if request.method=="POST":
     product = Product.objects.create()
     product.save()
-    return HttpResponse(product)
+    return JsonResponse(model_to_dict(product))
   else:
     return HttpResponse("Invalid")
 
-def activity(user_id, product_id, request):
+@csrf_exempt
+def activity(request, user_id, product_id):
   if request.method=="POST":
-    score = request.POST.score
+    score = request.POST["score"]
     activity = Activity.objects.create(user_id=user_id, product_id=product_id, score=score)
     activity.save()
     return HttpResponse(activity)
   else:
     return HttpResponse("Invalid")
 
-def rec(request):
-  if request.method=="POST":
-    user = User.objects.get(user_id=request.POST.user_id) 
-    game_recs =  Rec.objects.filter(player_id=player.id).exclude(game_id__in = Activity.objects.filter(player_id=player.id).values_list("game_id", flat=True)).distinct().order_by("score").select_related('game')[:20] 
-    product = Product.objects.create()
-    product.save()
-    return HttpResponse(product)
-  else:
-    return HttpResponse("Invalid")
-
+def rec(request, user_id):
+  user = User.objects.get(id=user_id) 
+  game_recs =  Rec.objects.filter(user_id=user_id).exclude(product_id__in = Activity.objects.filter(user_id=user.id).values_list("product_id", flat=True)).distinct().order_by("score").select_related('product')[:20] 
+  print {"recs":[model_to_dict(game) for game in game_recs]}
+  return JsonResponse({"recs":[model_to_dict(game) for game in game_recs]})
+    
--- svd/apps.py
+++ svd/apps.py
@@ -12,24 +12,25 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 
 pw = os.getenv("SQLPW")
 #pw = os.environ.get('SQLPW', False)
-conn = psycopg2.connect("host=localhost dbname=game_recs user=jcbraun password="+pw)
+conn = psycopg2.connect("host=localhost dbname=recs_engine user=jcbraun password="+pw)
 
 c = conn.cursor()
-c.execute('SELECT id FROM recs_player')
+c.execute('SELECT id FROM rec_app_user')
 users = []
 for row in c.fetchall():
   users.append(row[0])
-c.execute('SELECT game_id, name FROM recs_game')
+c.execute('SELECT id FROM rec_app_product')
 games = []
-named_games = []
 for row in c.fetchall():
   games.append(row[0])
-  named_games.append(row[1])
-c.execute('SELECT play_time, game_id, player_id FROM recs_activity')
+c.execute('SELECT score, product_id, user_id FROM rec_app_activity')
 matrix = [[0] * len(users) for game in games]
 for row in c.fetchall():
   matrix[games.index(row[1])][users.index(row[2])] = row[0]
-u,s,v=np.linalg.svd(matrix, full_matrices=True) 
+try:
+  u,s,v=np.linalg.svd(matrix, full_matrices=True) 
+except Exception as e:
+  print e 
 print "HERE "
 
 class SvdConfig(AppConfig):
